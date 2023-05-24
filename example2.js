ab13597d866870a63cb984284770ad31032a7176
Mukul0906 <122787724+Mukul0906@users.noreply.github.com>
2023-05-24 21:07:10 +0530

    Add files via upload

---
+++ example2.js
@@ -0,0 +1,10 @@
+var fname = "MUKUL";
+var lname = "KUMAR";
+var fullname = "".concat(fname) + " " + "".concat(lname);
+var fullname2 = fname + " " + lname;
+console.log(fname);
+console.log(lname);
+console.log(fullname);
+console.log(fullname2);
+console.log(fname.charAt(2));
+console.log(fullname.lastIndexOf('r'));
