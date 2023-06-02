76bdfcd6cad94c15531260df007ad98a4f0c3779
Mukul0906 <122787724+Mukul0906@users.noreply.github.com>
2023-06-02 20:00:57 +0530

    Add files via upload

---
+++ function.ts
@@ -0,0 +1,17 @@
+function fullName(fName:string,lName:string="Kumar"):string{
+    return fName+" "+lName;
+}
+console.log(fullName('Mukul'));
+//Arrow function
+let print1=()=>console.log("Welcome to batch 2pm");
+print1();
+
+/*function display(num:number){
+    if(num%2==0){
+        console.log("Even Number");
+    }else{
+        console.log("Odd Number");
+    }
+}
+console.log(display(23));*/
+
